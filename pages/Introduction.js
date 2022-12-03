fc5d8593e9f26fd7dc9ea673e1ded530484d844b
umaresso <umarkhatab467@gmail.com>
2022-12-03 18:19:00 +0500

    changed introduction page dummy image alt text

--- pages/Introduction.js
+++ pages/Introduction.js
@@ -30,7 +30,7 @@ export default function Introduction(props) {
               ? image
               : "https://camo.githubusercontent.com/0cbb62ec60fd23643b211d7ed0bb5ade6964f0da12124b95a0a3e7db096768c3/68747470733a2f2f7261772e6769746875622e636f6d2f74756c696f732f706c616365686f6c6465725f7368696d6d65722f6d61737465722f73616d706c652e676966"
           }
-          alt="bordape"
+          alt="loading collection"
         />
       </div>
     </div>
